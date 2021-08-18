89fc5a1884434a8f739c10473134dee271a85a66
fanghao <fanghao@ctirobot.com>
2021-08-18 12:44:20 +0800

    pre: update config

--- .umirc.ts
+++ .umirc.ts
@@ -8,4 +8,6 @@ export default defineConfig({
     { path: '/', component: '@/pages/index' },
   ],
   fastRefresh: {},
+  base: '/todos/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/todos/' : '/',
 });
